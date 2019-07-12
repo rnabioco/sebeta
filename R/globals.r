82781ac9c1e69da451e8ff794459c3a1a28cca16
Kent Riemondy <kent.riemondy@gmail.com>
2019-07-12 07:49:21 -0600

    update heatmaps and add volcano plots

--- R/globals.r
+++ R/globals.r
@@ -281,4 +281,101 @@ get_high_low <- function(vec, n){
   vec[c(1:split, (len_v - split):len_v)]
 }
 
-
+importCDS_v3 <- function (otherCDS, seurat_scale=F, import_all = FALSE)
+{
+  if (class(otherCDS)[1] == "Seurat") {
+    requireNamespace("Seurat")
+    if (!seurat_scale) {
+      data <- otherCDS@assays$RNA@counts
+    } else {
+      data <- otherCDS@assays$RNA@scale.data
+    }
+    if (class(data) == "data.frame") {
+      data <- as(as.matrix(data), "sparseMatrix")
+    }
+    pd <- tryCatch({
+      pd <- new("AnnotatedDataFrame", data = otherCDS@meta.data)
+      pd
+    }, error = function(e) {
+      pData <- data.frame(cell_id = colnames(data), row.names = colnames(data))
+      pd <- new("AnnotatedDataFrame", data = pData)
+      message("This Seurat object doesn't provide any meta data")
+      pd
+    })
+    if (length(setdiff(colnames(data), rownames(pd))) > 0) {
+      data <- data[, rownames(pd)]
+    }
+    fData <- data.frame(gene_short_name = row.names(data),
+                        row.names = row.names(data))
+    fd <- new("AnnotatedDataFrame", data = fData)
+    #lowerDetectionLimit <- otherCDS@is.expr
+    if (all(data == floor(data))) {
+      expressionFamily <- negbinomial.size()
+      expr <- "negbinomial.size"
+    }
+    else if (any(data < 0)) {
+      expressionFamily <- uninormal()
+      expr <- "unimormal"
+    }
+    else {
+      expressionFamily <- tobit()
+      expr <- "tobit"
+    }
+    print(paste0("expressionFamily ",expr))
+    # valid_data <- data[, row.names(pd)]
+    monocle_cds <- newCellDataSet(data, phenoData = pd, featureData = fd,
+                                  #lowerDetectionLimit = lowerDetectionLimit,
+                                  expressionFamily = expressionFamily)
+    if (import_all) {
+      if ("Monocle" %in% names(otherCDS@misc)) {
+        otherCDS@misc$Monocle@auxClusteringData$seurat <- NULL
+        otherCDS@misc$Monocle@auxClusteringData$scran <- NULL
+        monocle_cds <- otherCDS@misc$Monocle
+        mist_list <- otherCDS
+      }
+      else {
+        mist_list <- otherCDS
+      }
+    }
+    else {
+      mist_list <- list()
+    }
+    if ("var.genes" %in% slotNames(otherCDS)) {
+      var.genes <- setOrderingFilter(monocle_cds, otherCDS@var.genes)
+    }
+    monocle_cds@auxClusteringData$seurat <- mist_list
+  }
+  else if (class(otherCDS)[1] == "SCESet") {
+    requireNamespace("scater")
+    message("Converting the exprs data in log scale back to original scale ...")
+    data <- 2^otherCDS@assayData$exprs - otherCDS@logExprsOffset
+    fd <- otherCDS@featureData
+    pd <- otherCDS@phenoData
+    experimentData = otherCDS@experimentData
+    if ("is.expr" %in% slotNames(otherCDS))
+      lowerDetectionLimit <- otherCDS@is.expr
+    else lowerDetectionLimit <- 1
+    if (all(data == floor(data))) {
+      expressionFamily <- negbinomial.size()
+    }
+    else if (any(data < 0)) {
+      expressionFamily <- uninormal()
+    }
+    else {
+      expressionFamily <- tobit()
+    }
+    if (import_all) {
+      mist_list <- otherCDS
+    }
+    else {
+      mist_list <- list()
+    }
+    monocle_cds <- newCellDataSet(data, phenoData = pd, featureData = fd,
+                                  lowerDetectionLimit = lowerDetectionLimit, expressionFamily = expressionFamily)
+    monocle_cds@auxOrderingData$scran <- mist_list
+  }
+  else {
+    stop("the object type you want to export to is not supported yet")
+  }
+  return(monocle_cds)
+}
